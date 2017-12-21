58c1e92b2c6bed2b47ea0fc7d41242d8d1cd1795
sleepwalky <keeweery+github@gmail.com>
2017-12-21 22:40:32 +0300

    Fix cookie middleware

--- middlewares/cookieMiddleware.js
+++ middlewares/cookieMiddleware.js
@@ -1,7 +1,9 @@
 const cookie = require('cookie');
 
 module.exports = function (req, res, next) {
-  res.parsedCookies = cookie.parse(req.headers.cookie);
+  if (req.headers.cookie) {
+    res.parsedCookies = cookie.parse(req.headers.cookie);
+  }
 
   next();
 }
